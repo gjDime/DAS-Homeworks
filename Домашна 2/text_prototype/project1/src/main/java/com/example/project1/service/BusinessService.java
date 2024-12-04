3e43eb087a82b8c02aa4c751b98c1d0581215ec3
DanielTrajkovski <trajkovskidanielll@gmail.com>
2024-12-04 23:24:30 +0100

    Added homework 2

---
+++ Домашна 2/text_prototype/project1/src/main/java/com/example/project1/service/BusinessService.java
@@ -0,0 +1,32 @@
+package com.example.project1.service;
+
+import com.example.project1.model.BusinessEntity;
+import com.example.project1.model.PriceLogEntity;
+import com.example.project1.repository.PriceLogRepository;
+import com.example.project1.repository.BusinessRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BusinessService {
+
+    private final BusinessRepository businessRepository;
+    private final PriceLogRepository priceLogRepository;
+
+    public List<BusinessEntity> findAll() {
+        return businessRepository.findAll();
+    }
+
+    public BusinessEntity findById(Long id) throws Exception {
+        return businessRepository.findById(id).orElseThrow(Exception::new);
+    }
+
+    public List<PriceLogEntity> findAllToday() {
+        return priceLogRepository.findAllByDate(LocalDate.now());
+    }
+
+}
