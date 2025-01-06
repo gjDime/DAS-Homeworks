d9cdec7624b693444e2c5e14eba0e2d23ef26933
gjDime <dime.gorgiev.03@gmail.com>
2025-01-06 22:22:00 +0100

    Added FilterUtils

--- Domashna 4/project1/src/main/java/com/example/project1/data/pipeline/impl/FilterPriceLog.java
+++ Domashna 4/project1/src/main/java/com/example/project1/data/pipeline/impl/FilterPriceLog.java
@@ -1,52 +1,47 @@
 package com.example.project1.data.pipeline.impl;
 
-import com.example.project1.data.DataParser;
+import com.example.project1.data.FilterUtils;
 import com.example.project1.data.pipeline.Filter;
 import com.example.project1.model.BusinessEntity;
-import com.example.project1.model.PriceLogEntity;
 import com.example.project1.repository.BusinessRepository;
 import com.example.project1.repository.PriceLogRepository;
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 
 public class FilterPriceLog implements Filter<List<BusinessEntity>> {
-
-    private final BusinessRepository businessRepository;
-    private final PriceLogRepository priceLogRepository;
-
-    private static final String HISTORICAL_DATA_URL = "https://www.mse.mk/mk/stats/symbolhistory/";
+    private final FilterUtils filterUtils;
 
     public FilterPriceLog(BusinessRepository businessRepository, PriceLogRepository priceLogRepository) {
-        this.businessRepository = businessRepository;
-        this.priceLogRepository = priceLogRepository;
+        filterUtils = new FilterUtils(businessRepository, priceLogRepository);
+    }
+
+    private List<BusinessEntity> filterInput(List<BusinessEntity> input) {
+        List<String> toIgnoreIssuers = Arrays.asList("BLTU", "CBNG", "ELNC", "ENSA", "INHO", "INTP", "KULT", "LAJO", "MPTE", "MZHE",
+                "OBPP", "OTEK", "SPOL", "TASK", "TIGA", "TRDB", "VFPM", "ZILUP");
+
+        //filter input issuers
+        return input.stream()
+                .filter(i -> !toIgnoreIssuers.contains(i.getCompanyCode()))
+                .toList();
     }
 
     public List<BusinessEntity> execute(List<BusinessEntity> input) throws IOException {
+        System.out.println("filter2 started");
+        input = filterInput(input);
+
+
         List<BusinessEntity> companies = new ArrayList<>();
-        List<String> toIgnore = Arrays.asList("BLTU", "CBNG", "ELNC", "ENSA", "INHO", "INTP", "KULT", "LAJO", "MPTE", "MZHE",
-                "OBPP", "OTEK", "SPOL", "TASK", "TIGA", "TRDB", "VFPM", "ZILUP");
-        input = input.stream()
-                .filter(i -> !toIgnore.contains(i.getCompanyCode()))
-                .toList();
         for (BusinessEntity company : input) {
-            if (company.getLastUpdated() == null) {
-                System.out.println("KOMPANI PROCESSING NOW: " + company.getCompanyCode());
+            if (company.getLastUpdated() == null) {//fetch last 10 years
+                System.out.println("COMPANY PROCESSING " + company.getCompanyCode());
                 for (int i = 1; i <= 10; i++) {
-                    int temp = i - 1;
                     LocalDate fromDate = LocalDate.now().minusYears(i);
-                    LocalDate toDate = LocalDate.now().minusYears(temp);
-                    addHistoricalData(company, fromDate, toDate);
+                    LocalDate toDate = LocalDate.now().minusYears(i - 1);
+                    filterUtils.addHistoricalData(company, fromDate, toDate);
                 }
             } else {
                 companies.add(company);
@@ -55,60 +50,4 @@ public class FilterPriceLog implements Filter<List<BusinessEntity>> {
 
         return companies;
     }
-
-    private void addHistoricalData(BusinessEntity company, LocalDate fromDate, LocalDate toDate) throws IOException {
-        Connection.Response response = Jsoup.connect(HISTORICAL_DATA_URL + company.getCompanyCode())
-                .data("FromDate", fromDate.toString())
-                .data("ToDate", toDate.toString())
-                .method(Connection.Method.POST)
-                .execute();
-
-        Document document = response.parse();
-
-        Element table = document.select("table#resultsTable").first();
-
-        if (table != null) {
-            Elements rows = table.select("tbody tr");
-
-            for (Element row : rows) {
-                Elements columns = row.select("td");
-
-                if (columns.size() > 0) {
-                    LocalDate date = DataParser.parseDate(columns.get(0).text(), "d.M.yyyy");
-
-                    if (priceLogRepository.findByDateAndCompany(date, company).isEmpty()) {
-
-
-                        NumberFormat format = NumberFormat.getInstance(Locale.GERMANY);
-
-                        Double lastTransactionPrice = DataParser.parseDouble(columns.get(1).text(), format);
-                        Double maxPrice = DataParser.parseDouble(columns.get(2).text(), format);
-                        Double minPrice = DataParser.parseDouble(columns.get(3).text(), format);
-                        Double averagePrice = DataParser.parseDouble(columns.get(4).text(), format);
-                        Double percentageChange = DataParser.parseDouble(columns.get(5).text(), format);
-                        Integer quantity = DataParser.parseInteger(columns.get(6).text(), format);
-                        Integer turnoverBest = DataParser.parseInteger(columns.get(7).text(), format);
-                        Integer totalTurnover = DataParser.parseInteger(columns.get(8).text(), format);
-
-                        if (maxPrice != null) {
-
-                            if (company.getLastUpdated() == null || company.getLastUpdated().isBefore(date)) {
-                                company.setLastUpdated(date);
-                            }
-
-                            PriceLogEntity priceLogEntity = new PriceLogEntity(
-                                    date, lastTransactionPrice, maxPrice, minPrice, averagePrice, percentageChange,
-                                    quantity, turnoverBest, totalTurnover);
-                            priceLogEntity.setCompany(company);
-                            priceLogRepository.save(priceLogEntity);
-                            company.getHistoricalData().add(priceLogEntity);
-                        }
-                    }
-                }
-            }
-        }
-
-        businessRepository.save(company);
-    }
-
 }
--- Domashna 4/project1/src/main/java/com/example/project1/data/pipeline/impl/FilterReAddPrice.java
+++ Domashna 4/project1/src/main/java/com/example/project1/data/pipeline/impl/FilterReAddPrice.java
@@ -1,99 +1,30 @@
 package com.example.project1.data.pipeline.impl;
 
-import com.example.project1.data.DataParser;
+import com.example.project1.data.FilterUtils;
 import com.example.project1.data.pipeline.Filter;
 import com.example.project1.model.BusinessEntity;
-import com.example.project1.model.PriceLogEntity;
 import com.example.project1.repository.BusinessRepository;
 import com.example.project1.repository.PriceLogRepository;
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.text.NumberFormat;
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Locale;
 
 public class FilterReAddPrice implements Filter<List<BusinessEntity>> {
-
-    private final BusinessRepository businessRepository;
-    private final PriceLogRepository priceLogRepository;
-
-    private static final String HISTORICAL_DATA_URL = "https://www.mse.mk/mk/stats/symbolhistory/";
+    private final FilterUtils filterUtils;
 
     public FilterReAddPrice(BusinessRepository businessRepository, PriceLogRepository priceLogRepository) {
-        this.businessRepository = businessRepository;
-        this.priceLogRepository = priceLogRepository;
+        filterUtils = new FilterUtils(businessRepository, priceLogRepository);
     }
 
-    public List<BusinessEntity> execute(List<BusinessEntity> input) throws IOException, ParseException {
-
+    public List<BusinessEntity> execute(List<BusinessEntity> input) throws IOException {
+        System.out.println("filter3 started");
         for (BusinessEntity company : input) {
-            LocalDate fromDate = LocalDate.now();
-            LocalDate toDate = LocalDate.now().plusYears(1);
-            addHistoricalData(company, fromDate, toDate);
+            LocalDate fromDate = company.getLastUpdated();
+            LocalDate toDate = LocalDate.now();
+            filterUtils.addHistoricalData(company, fromDate, toDate);
         }
 
         return null;
     }
-
-    private void addHistoricalData(BusinessEntity company, LocalDate fromDate, LocalDate toDate) throws IOException {
-        Connection.Response response = Jsoup.connect(HISTORICAL_DATA_URL + company.getCompanyCode())
-                .data("FromDate", fromDate.toString())
-                .data("ToDate", toDate.toString())
-                .method(Connection.Method.POST)
-                .execute();
-
-        Document document = response.parse();
-
-        Element table = document.select("table#resultsTable").first();
-
-        if (table != null) {
-            Elements rows = table.select("tbody tr");
-
-            for (Element row : rows) {
-                Elements columns = row.select("td");
-
-                if (columns.size() > 0) {
-                    LocalDate date = DataParser.parseDate(columns.get(0).text(), "d.M.yyyy");
-
-                    if (priceLogRepository.findByDateAndCompany(date, company).isEmpty()) {
-
-                        NumberFormat format = NumberFormat.getInstance(Locale.GERMANY);
-
-                        Double lastTransactionPrice = DataParser.parseDouble(columns.get(1).text(), format);
-                        Double maxPrice = DataParser.parseDouble(columns.get(2).text(), format);
-                        Double minPrice = DataParser.parseDouble(columns.get(3).text(), format);
-                        Double averagePrice = DataParser.parseDouble(columns.get(4).text(), format);
-                        Double percentageChange = DataParser.parseDouble(columns.get(5).text(), format);
-                        Integer quantity = DataParser.parseInteger(columns.get(6).text(), format);
-                        Integer turnoverBest = DataParser.parseInteger(columns.get(7).text(), format);
-                        Integer totalTurnover = DataParser.parseInteger(columns.get(8).text(), format);
-
-                        if (maxPrice != null) {
-
-                            if (company.getLastUpdated() == null || company.getLastUpdated().isBefore(date)) {
-                                company.setLastUpdated(date);
-                            }
-
-                            PriceLogEntity priceLogEntity = new PriceLogEntity(
-                                    date, lastTransactionPrice, maxPrice, minPrice, averagePrice, percentageChange,
-                                    quantity, turnoverBest, totalTurnover);
-                            priceLogEntity.setCompany(company);
-                            priceLogRepository.save(priceLogEntity);
-                            company.getHistoricalData().add(priceLogEntity);
-                        }
-                    }
-                }
-            }
-        }
-        businessRepository.save(company);
-    }
-
-
 }
